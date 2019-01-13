6e4c5b9dcf9679b3b33d8d2671b2c096180c82df
AP Ljungquist <ap@ljungquist.eu>
2019-01-13 20:58:46 +0100

    Adds entry_points experiment

--- tests/test_xfmt.py
+++ tests/test_xfmt.py
@@ -1,2 +1,6 @@
-def test_noop():
-    print("These are not the droids you are looking for")
+"""
+This is for pytest to find and stop being upset not finding any tests.
+
+>>> 'Happy?'[:-1]
+'Happy'
+"""
