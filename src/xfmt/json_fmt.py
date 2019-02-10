e59c0983f222e81cfe8bbd10e10e3d281c0c4e42
AP Ljungquist <ap@ljungquist.eu>
2019-02-10 12:49:34 +0100

    Moves function to misc module
    ... because it will soon be reused.

--- src/xfmt/json_fmt.py
+++ src/xfmt/json_fmt.py
@@ -1,45 +1,18 @@
 """
 Formatting functions for json
 """
-import difflib
 import json
 import os
-from typing import Iterable, List, Optional
+from typing import List
 
 from xfmt import base
+from xfmt.misc import diff
 
 JSON_PRETTY_KWARGS = {
     'indent': 2,
     'separators': (',', ': '),
     'sort_keys': True,
 }
-
-
-def _chunk_lines(lines: Iterable[str], fromfile: str) -> Iterable[str]:
-    chunk = []  # type: List[str]
-    for line in lines:
-        if line == '--- {}\n'.format(fromfile):
-            yield ''.join(chunk)
-            chunk = []
-        chunk.append(line)
-    yield ''.join(chunk)
-
-
-def _diff(before: str, after: str, path: Optional[str] = None) -> List[str]:
-    if path is None:
-        fromfile = 'actual'
-        tofile = 'expected'
-    else:
-        fromfile = tofile = path
-
-    lines = difflib.unified_diff(
-        before.splitlines(keepends=True),
-        after.splitlines(keepends=True),
-        fromfile=fromfile,
-        tofile=tofile
-    )
-    chunks = list(_chunk_lines(lines, fromfile))
-    return chunks[1:]  # First chunk is empty
 
 
 def fix_content_json(before: str) -> str:
@@ -56,7 +29,7 @@ def check_content_json(before: str) -> List[str]:
     after = fix_content_json(before)
     if before == after:
         return []
-    return _diff(before, after)
+    return diff(before, after)
 
 
 def fix_file_json(path: str) -> List[str]:
@@ -73,7 +46,7 @@ def fix_file_json(path: str) -> List[str]:
     with open(path, 'w') as fp:
         fp.write(after)
 
-    return _diff(before, after)
+    return diff(before, after)
 
 
 def check_file_json(path: str) -> List[str]:
@@ -86,7 +59,7 @@ def check_file_json(path: str) -> List[str]:
     if before == after:
         return []
 
-    return _diff(before, after, path)
+    return diff(before, after, path)
 
 
 class JsonFormatter(base.Formatter):
--- src/xfmt/misc.py
+++ src/xfmt/misc.py
@@ -1,3 +1,34 @@
 """
 Miscellaneous utilities
 """
+import difflib
+from typing import Iterable, List, Optional
+
+
+def _chunk_lines(lines: Iterable[str], fromfile: str) -> Iterable[str]:
+    chunk = []  # type: List[str]
+    for line in lines:
+        if line == '--- {}\n'.format(fromfile):
+            yield ''.join(chunk)
+            chunk = []
+        chunk.append(line)
+    yield ''.join(chunk)
+
+
+def diff(before: str, after: str, path: Optional[str] = None) -> List[str]:
+    """Produce feedback from the difference between two versions.
+    """
+    if path is None:
+        fromfile = 'actual'
+        tofile = 'expected'
+    else:
+        fromfile = tofile = path
+
+    lines = difflib.unified_diff(
+        before.splitlines(keepends=True),
+        after.splitlines(keepends=True),
+        fromfile=fromfile,
+        tofile=tofile
+    )
+    chunks = list(_chunk_lines(lines, fromfile))
+    return chunks[1:]  # First chunk is empty
